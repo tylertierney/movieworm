85ef3ba78edf06da882a165ef727b3948dbfe7d5
Tyler Tierney <tytierney@yahoo.com>
2021-10-21 16:31:36 -0400

    Updating readme and next config

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MovieWorm
 
 This is a site where users can create or join private groups and review movies with their friends.
+
+Live Site: www.movieworm.io
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   reactStrictMode: true,
   env: {
     api_key: "8d4d1ae4d36603cf91dacea6e5205197",
   },
-};
+});
